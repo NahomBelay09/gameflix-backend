b44a22c118a56fa0bf9f594d6a7724db4d97de20
NahomBelay09 <215428397+NahomBelay09@users.noreply.github.com>
2025-07-30 00:24:59 -0400

    Add GitHub Actions CI workflow

---
+++ Desktop/gameflix backend 2/src/test/java/gameflix__backend_/GameflixBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package gameflix__backend_;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameflixBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
