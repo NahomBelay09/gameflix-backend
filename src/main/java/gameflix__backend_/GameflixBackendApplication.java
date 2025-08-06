44e489e7441756b4310cfd320bd2a66701c3803c
NahomBelay09 <215428397+NahomBelay09@users.noreply.github.com>
2025-08-06 01:28:42 -0400

    Initial commit with CI

---
+++ src/main/java/gameflix__backend_/GameflixBackendApplication.java
@@ -0,0 +1,13 @@
+package gameflix__backend_;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameflixBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameflixBackendApplication.class, args);
+    }
+
+}
